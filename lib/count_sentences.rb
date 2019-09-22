151de291afa43507216e9718d0f20225f4d705d7
Soumya_Goyal <somyagoyal739@gmail.com>
2019-09-22 14:08:26 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,21 @@
 require 'pry'
 
 class String
-
+  attr_reader :sentence
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    string_array = self.split(/[\!?.]\s+/)
+    string_array.count
   end
 end
